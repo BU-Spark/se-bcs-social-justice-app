fdee8fc1b4ff618300c9f920cfce8956b91ae908
Sean Lin <linsean@bu.edu>
2025-03-02 16:06:28 -0500

    updated signin/signup pages and database to include onboarding boolean

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -22,6 +22,7 @@ export const checkUser = async () => {
       name: `${user.firstName} ${user.lastName}`,
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress,
+      onboardingComplete: false,
     },
   });
 
