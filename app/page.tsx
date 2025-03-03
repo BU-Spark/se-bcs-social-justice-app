2f1b7d1992f05f4501753938bdff3740fe4b9bfe
suranabhavya <bhavyasurana2000@gmail.com>
2025-03-03 09:42:53 -0500

    updating prettier changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "next/navigation";
 import Sidebar from "./components/Sidebar";
 import Onboarding from "./components/Onboarding";
 import { useUser } from "@clerk/nextjs";
-import { checkUser } from "@/lib/checkUser";
+
 const HomePage = () => {
   const searchParams = useSearchParams();
   const fromSignup = searchParams.get("from") === "signup";
@@ -16,7 +16,7 @@ const HomePage = () => {
         {fromSignup && <Onboarding />}
         <Sidebar />
         <h1 className="text-2xl font-bold">
-          Welcome to the Social Justice Platform 
+          Welcome to the Social Justice Platform
         </h1>
         <p>Signed in as {user?.fullName}</p>
       </div>
